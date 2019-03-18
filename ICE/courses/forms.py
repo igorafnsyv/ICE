be3556a8dd3be07dcde38d143712a0941e575741
Igor Afanasyev <igor.afanasyev.msk@gmail.com>
2019-03-19 02:27:44 +0800

    first commit

---
+++ ICE/courses/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import Course, Module
+from django.core.exceptions import ValidationError
+
+class ModuleForm(forms.ModelForm):
+    class Meta:
+        model = Module
+        fields = ['title', 'slug']
+
+
+
+   # def save(self):
+        #course = 
+    #    new_module = Module.objects.create(title = self.cleaned_data['title'], slug = self.cleaned_data['slug'], course = self.cleaned_data['course'])
+     #   return new_module
