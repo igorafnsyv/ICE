86dc37deeef521475734db90f062be5478c1597c
Igor Afanasyev <igor.afanasyev.msk@gmail.com>
2019-03-27 16:04:05 +0800

    Fixed problem with static files

--- ICE/config/settings.py
+++ ICE/config/settings.py
@@ -122,5 +122,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
